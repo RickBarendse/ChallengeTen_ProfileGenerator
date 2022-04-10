ee5f7a5841ebf91ca1d3cd87fa53ad75072a0241
Rick Barendse <dazedeyes@yahoo.com>
2022-04-10 13:49:31 -0700

    manager constuctor

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
+const fs = require('fs');
 const inquirer = require('inquirer');
 const generatePage = require('./src/page-template');
 const { writeFile, copyFile } = require('./utils/generate-site');
 
-const promptUser = () => {
+const Manager = require('./lib/Manager');
+
+
+const teamArray = [];
+
+
+const promptManager = () => {
     return inquirer.prompt([
         {
             type: 'input',
             name: 'name',
-            message: 'Please enter employee name. (required)',
+            message: "Please enter the manager's name. (required)",
             validate: nameInput => {
                 if (nameInput) {
                     return true;
                 } else {
-                    console.log('Employee name is required!');
+                    console.log('Manager name is required!');
                     return false;
                 }
             }
@@ -21,9 +28,108 @@ const promptUser = () => {
         {
             type: 'input',
             name: 'id',
-            message: 'Please enter employee ID. (required)',
+            message: "Please enter the manager's employee ID. (required)",
             validate: idInput => {
                 if (idInput) {
+                    return true;
+                } else {
+                    console.log('Manager ID is required!')
+                    return false;
+                }
+            }
+        },
+
+        {
+            type: 'input',
+            name: 'email',
+            message: "Enter manager's email address. (required)'",
+            validate:  emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    console.log('Email address is required!')
+                    return false;
+                }
+            }
+        },
+
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: "Enter the manager's office number. (required)",
+            validate: officeNumberInput => {
+                if (officeNumberInput) {
+                    return true;
+                } else {
+                    console.log('Office number is required')
+                    return false;
+                }
+            }
+        },
+
+        {
+            type: 'confirm',
+            name: 'confirmTeam',
+            message: 'Would you like to add team members?',
+            default: true
+        },
+          
+        {
+            type: 'list',
+            name: 'roleList',
+            message: "What is the employee's role? (required",
+            choices: ['Engineer', 'Intern'],
+            when: ({ confirmTeam }) => {
+                if (confirmTeam) {
+                    return true 
+                } else {
+                    return false;
+                }
+            }
+        }
+    ])
+
+    .then(managerData => {
+        const { managerName, managerId, managerEmail, officeNumber } = managerData;
+        const manager = new Manager (managerName, managerId, managerEmail, officeNumber);
+        teamArray.push(manager);
+
+        if (managerData.confirmTeam) {
+            return addEmployee();
+        } else {
+            return
+        }
+    })
+};
+
+const addEmployee = employeeData => {
+    console.log(
+        `========================
+         Add a employee to roster
+         ========================
+        `);
+
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'employeeName',
+            message: "What is the employee's name? (required)",
+            validat: employeeNameInput => {
+                if (employeeNameInput) {
+                    return true;
+                } else {
+                    console.log('You must provide an employee name!')
+                    return false;
+                }  
+            }      
+        },
+
+        {
+            type: 'input',
+            name: 'employeeId',
+            message: "Please enter the employee ID. (required)",
+            validate: employeeIdInput => {
+                if (employeeIdInput) {
                     return true;
                 } else {
                     console.log('Employee ID is required!')
@@ -34,23 +140,23 @@ const promptUser = () => {
 
         {
             type: 'input',
-            name: 'email',
-            message: 'Enter employee email address. (required)',
-            validate: emailInput => {
-                if (emailInput) {
+            name: 'employeeEmail',
+            message: "Enter employee's email address. (required)",
+            validate:  employeeEmailInput => {
+                if (employeeEmailInput) {
                     return true;
                 } else {
                     console.log('Email address is required!')
                     return false;
                 }
-            }
-        }
+            }    
+        },
     ])
 }
 
-promptUser()
-    .then(promptUser => {
-    return generatePage(promptUser);
+promptManager()
+    .then(promptManager => {
+    return generatePage(promptManager);
 })
 .then(pageHTML => {
     return writeFile (pageHTML);
